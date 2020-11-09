670e465793401fd95b12066ce9ef76abd214d167
derekmccool <mccool.derek@yahoo.com>
2020-11-08 23:29:35 -0600

    styling on projects

--- JS Portfolio Project/js/projects.js
+++ JS Portfolio Project/js/projects.js
@@ -1,20 +1,12 @@
 function toggleExpand(event){
     let val = document.getElementById(event.target.value);
-    console.log(val.parentNode);
     if(val.className === "project-collapse"){
         val.setAttribute("class", "project-expand");
         console.log("expanded");
         event.target.innerHTML = "Collapse";
-        val.parentNode.style.display = "flex";
-        val.parentNode.style.height = "700px";
-        console.log(val.parentNode.firstElementChild);
-        
 
     }else{
         val.setAttribute("class", "project-collapse");
-        console.log("collapsed");
         event.target.innerHTML = "See Details";
-        val.parentNode.style.display = "block";
-
     }
 }
