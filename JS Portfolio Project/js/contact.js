7689512582b1ac62824e7d75f3911952c0091ee5
derekmccool <mccool.derek@yahoo.com>
2020-11-09 00:44:29 -0600

    contact styling

--- JS Portfolio Project/js/contact.js
+++ JS Portfolio Project/js/contact.js
@@ -1,14 +1,10 @@
 var count = 1;
-
 const addContact = event => {
-
     event.preventDefault();
-
     const nameDiv = addNameDiv(count, document.getElementById("name").value);
     const emailDiv = addEmailDiv(count, document.getElementById("email").value);
-
-    contactDiv.append(addDivRow(count,nameDiv, emailDiv));
-
+    const timeDiv = addTimeDiv()
+    contactDiv.append(addDivRow(count,nameDiv, emailDiv, timeDiv));
     contactForm.reset();
     count++;
 }
@@ -18,24 +14,22 @@ const contactMe = document.getElementById("contact-me");
 
 contactMe.addEventListener("click", addContact);
 
-function addDivRow(id, nameDiv, emailDiv){
+function addDivRow(id, nameDiv, emailDiv, timeDiv){
     let row = document.createElement("div");
     row.setAttribute("class", "contact-row");
     row.setAttribute("id", `contact-${id}`);
     row.append(nameDiv);
     row.append(emailDiv);
+    row.append(timeDiv)
     row.append(addBtnDiv(id));
     return row;
 }
 
 function addNameDiv(id, name){
-
     let rowName = document.createElement("div");
     rowName.setAttribute("class", "name-div");
     rowName.setAttribute("id", `name-${id}`);
-
     rowName.innerHTML = name;
-
     return rowName;
 }
 
@@ -44,8 +38,15 @@ function addEmailDiv(id, email){
     rowEmail.setAttribute("class", "email-div");
     rowEmail.setAttribute("id", `email-${id}`);
     rowEmail.innerHTML = email;
-    
     return rowEmail;
+}
+
+function addTimeDiv(){
+    let date = new Date().toUTCString();
+    let rowTimeStamp = document.createElement("div");
+    rowTimeStamp.setAttribute("class", "time-div");
+    rowTimeStamp.innerHTML = date;
+    return rowTimeStamp;
 }
 
 function addBtnDiv(id){
@@ -54,16 +55,16 @@ function addBtnDiv(id){
     rowButtons.setAttribute("class", "button-div");
     
     let deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("class", "button-div");
+    deleteBtn.setAttribute("class", "delete-btn");
     deleteBtn.setAttribute("value", id);
     deleteBtn.addEventListener("click", deleteRow);
-    deleteBtn.innerHTML = "Delete";
+    deleteBtn.innerHTML = "X";
 
     let messageBtn = document.createElement("button");
-    messageBtn.setAttribute("class", "button-div");
+    messageBtn.setAttribute("class", "message-btn");
     messageBtn.setAttribute("value", id);
     messageBtn.addEventListener("click", messageContact);
-    messageBtn.innerHTML = "Send Message";
+    messageBtn.innerHTML = "message";
 
     rowButtons.append(deleteBtn);
     rowButtons.append(messageBtn);
@@ -74,10 +75,11 @@ function addBtnDiv(id){
 
 function deleteRow(event){
     document.getElementById(`contact-${event.target.value}`).remove();
-    
 }
 
 function messageContact(event){
-    console.log(name);
-    alert(`message sent to ${name.innerHTML}`);
+    
+    let val = event.target.value;
+    let name = document.getElementById(`name-${val}`).innerHTML;
+    alert("Message sent to " + name);
 }
